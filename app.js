ac341e88f783de484b2a80fa59e3b198978e40aa
Joabyson <joabyson916@gmail.com>
2025-03-27 22:50:05 -0300

    Feat: "alterando as variáveis de const para let , criando a função calcularJuros e enviando para o localStorage "

--- app.js
+++ app.js
@@ -2,22 +2,30 @@ const inputNome = document.querySelector(".nome");
 const inputMensalidade = document.querySelector(".mensalidade");
 const selectTempo = document.querySelector(".tempo");
 
-const nome = "";
-const mensalidade = "";
-const tempo = "";
+let nome = "";
+let mensalidade = 0;
+let tempo = 0;
+const taxadeJuros = 0.00517;
 
 inputNome.addEventListener("change", (event) => {
   nome = event.target.value;
-
 });
 
 inputMensalidade.addEventListener("change", (event) => {
-  mensalidade = event.target.value;
-
- 
+  mensalidade = parseFloat(event.target.value);
 });
 
 selectTempo.addEventListener("change", (event) => {
-  tempo = event.target.value;
-
+  tempo = parseInt(event.target.value) * 12;
 });
+
+function calcularJuros() {
+  const resultado =
+    mensalidade * (((1 + taxadeJuros) ** tempo - 1) / taxadeJuros);
+  localStorage.setItem("nome", nome);
+  localStorage.setItem("mensalidade", mensalidade);
+  localStorage.setItem("tempo", tempo);
+  localStorage.setItem("resultado", resultado);
+
+  window.location.href = "pagina-resultado.html";
+}
