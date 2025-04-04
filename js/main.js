ae08b90e3c00375c255079c76a20905312669a53
Joabyson <joabyson916@gmail.com>
2025-04-03 23:46:33 -0300

    Feat: "armazenando os dados No localStorage e criando a função voltar para a pagina principal"

--- js/main.js
+++ js/main.js
@@ -19,13 +19,28 @@ selectTempo.addEventListener("change", (event) => {
   tempo = parseInt(event.target.value) * 12;
 });
 
-function calcularJuros() {
-  const resultado =
-    mensalidade * (((1 + taxadeJuros) ** tempo - 1) / taxadeJuros);
-  localStorage.setItem("nome", nome);
-  localStorage.setItem("mensalidade", mensalidade);
-  localStorage.setItem("tempo", tempo);
-  localStorage.setItem("resultado", resultado);
 
-  window.location.href = "pagina-resultado.html";
+async function calcularJuros() {
+  const expr = `${mensalidade} * (((1 + ${taxadeJuros}) ^ ${tempo} - 1) / ${taxadeJuros})`;
+  const resultado = mensalidade * (((1 + taxadeJuros) ^ tempo - 1) / taxadeJuros).toFixed(3);
+  try {
+    console.log(resultado)
+    const resposta = await fetch("http://api.mathjs.org/v4/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ expr }),
+    });
+     localStorage.setItem("nome",nome)
+     localStorage.setItem("mensalidade",mensalidade)
+     localStorage.setItem("tempo",tempo)
+     localStorage.setItem("Taxa de juros",taxadeJuros)
+     localStorage.setItem("resultado", resultado)
+
+  } catch (error) {
+    console.log("Ops aconteceu algum erro", error);
+  }
+}
+
+function voltar() {
+  return (window.location.href = "http://127.0.0.1:5500/index.html");
 }
