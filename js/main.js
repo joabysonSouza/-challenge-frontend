690900e68a2fbc81eafc1bc89b5f6094d7587fb2
Joabyson <joabyson916@gmail.com>
2025-04-08 22:54:55 -0300

    Refactor :"alterando local da função voltar "

--- js/main.js
+++ js/main.js
@@ -22,25 +22,25 @@ selectTempo.addEventListener("change", (event) => {
 
 async function calcularJuros() {
   const expr = `${mensalidade} * (((1 + ${taxadeJuros}) ^ ${tempo} - 1) / ${taxadeJuros})`;
-  const resultado = mensalidade * (((1 + taxadeJuros) ^ tempo - 1) / taxadeJuros).toFixed(3);
+  const resultado = mensalidade * (((1 + taxadeJuros) ^ tempo - 1) / taxadeJuros).toFixed(2);
   try {
-    console.log(resultado)
+ 
     const resposta = await fetch("http://api.mathjs.org/v4/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ expr }),
     });
-     localStorage.setItem("nome",nome)
+
+     localStorage.setItem("usuario",nome)
      localStorage.setItem("mensalidade",mensalidade)
      localStorage.setItem("tempo",tempo)
      localStorage.setItem("Taxa de juros",taxadeJuros)
      localStorage.setItem("resultado", resultado)
+     window.location.href = "http://127.0.0.1:5500/pagina-resultado.html"
 
   } catch (error) {
     console.log("Ops aconteceu algum erro", error);
   }
 }
 
-function voltar() {
-  return (window.location.href = "http://127.0.0.1:5500/index.html");
-}
+
