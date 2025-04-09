9df4eed70de33c89b168efcf291a478df8d72776
Joabyson <joabyson916@gmail.com>
2025-04-08 22:53:46 -0300

    Feat : "pegando resultados do localStorage e exibindo na Pagina-resultado"

---
+++ js/pagina-resultado.js
@@ -0,0 +1,23 @@
+let nomeStorage = localStorage.getItem("usuario")
+let resultadoStorage = localStorage.getItem("resultado")
+let mensalidadeStorage = localStorage.getItem("mensalidade")
+let tempoStorage = localStorage.getItem("tempo")
+
+
+const nomeResultado= document.querySelector(".nome")
+const resultado= document.querySelector(".resultado")
+const mensalidadeResultado= document.querySelector(".mensalidade")
+const tempoResultado= document.querySelector(".tempo")
+
+
+
+
+nomeResultado.textContent = `${nomeStorage}`
+resultado.textContent = `${resultadoStorage}`
+mensalidadeResultado.textContent = `${mensalidadeStorage}`
+tempoResultado.textContent = `${tempoStorage}`
+
+
+function voltar() {
+    return (window.location.href = "http://127.0.0.1:5500/index.html");
+  }
